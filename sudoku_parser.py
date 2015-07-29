0eb128e728ba17c0be72416f0c0d2dfc4d4fce8e
Lantana <lanata3484@gmail.com>
2015-07-29 15:09:25 -0700

    Element clause, row clause, and block clause done. Remember to uncomment lines at the bottom of the file. Left testing lines in will add todo to remove them in final product.

--- sudoku_parser.py
+++ sudoku_parser.py
@@ -15,7 +15,7 @@ class SudokuParser(object):
         file = open(filename)
         initial_puzzle = file.readline()
         initial_puzzle = initial_puzzle.strip()
-	    puzzle = list(initial_puzzle)
+        puzzle = list(initial_puzzle)
         for n, item in enumerate(puzzle):
             if item == '.':
                 puzzle[n] = '0'
@@ -43,12 +43,13 @@ class SudokuParser(object):
         """
         if val == 0:
             return 0
+
         digit = 81*(row-1)+9*(col-1)+(val-1)+1
         return digit
     
     def encode(self, puzzle):
         table = sudoku.create_variable_table(puzzle)
-	    encoded = []
+        encoded = []
         for n, item in enumerate(puzzle):
             nums = list(table[n])
             i = int(nums[0])
@@ -64,12 +65,12 @@ class SudokuParser(object):
         num = ((val % 81) % 9)
         col = (((val % 81) - num) / 9)
         row = ((val - col - num) / 81)
-	    k = num + 1
-	    j = col + 1
+        k = num + 1
+        j = col + 1
         i = row + 1
         cell = str(i) + str(j)  
-	    decoded.append(cell)
-	    decoded.append(k)
+        decoded.append(cell)
+        decoded.append(k)
         return decoded
 
     def element_clauses(self):
@@ -77,73 +78,81 @@ class SudokuParser(object):
 
         :return:
         """
+        #print("Element Clauses") # for testing
         line = ''
         for i in range(1, 10):
             for j in range(1, 10):
                 for d in range(1, 10):
                     value = self.get_base_nine_num(i, j, d)
-                    line += str(value) + ' '
-                line += str(0)
-                #TODO: Eventually output this to our cnf file
-                #print(line)
+                    new_val = '{} '.format(value)
+                    line += new_val
+                line += '0\n'
+        #TODO: Eventually output this to our cnf file
+        #print(line) # for testing
 
     def row_clause(self):
         """
 
         :return:
         """
+        #print("Row Clauses") # for testing
         line = ''
         for i in range(1, 10):
             for j in range(1, 9):
                 for l in range(j+1, 10):
-                    line = ''
                     for d in range(1, 10):
                         literal1 = self.get_base_nine_num(i, j, d)
                         literal2 = self.get_base_nine_num(i, l, d)
-                        line += '-' + str(literal1) \
-                                + ' ' + '-'+str(literal2) \
-                                + ' ' + str(0) + '\n'
-                    #TODO: Eventually output this to our cnf file
-                    #print(line)
+                        literals = '-{0} -{1} 0\n'.format(literal1, literal2)
+                        line += literals
+        #TODO: Eventually output this to our cnf file
+        #print(line) # for testing
 
     def sub_grid_clause(self):
         """
 
         :return:
         """
-
-    for d in range(1, 10):
-            for a in range(0, 3):
-                for b in range(0, 3):
-                    for u in range(1, 4):
-                        for v in range(1, 3):
-                            line = ''
-                            for w in range(v + 1, 4):
-                                val1 = (3*a) + u
-                                val2 = (3*b) + v
-                                val3 = (3*b) + w
-                                lit1 = self.get_base_nine_num(val1, val2, d)
-                                lit2 = self.get_base_nine_num(val1, val2, d)
-                                literals = "-{0} -{1}".format(lit1, lit2)
-                                line += literals
-
-    for d in range(1, 10):
-            for a in range(0, 3):
-                for b in range(0, 3):
-                    for u in range(1, 3):
-                        for v in range(1, 4):
-                            line = ''
-                            for w in range(u + 1, 4):
-                                for t in range(1, 4):
+        # print("Sub Grid Clauses: ") # for testing
+        line = ''
+        for d in range(1, 10):
+                for a in range(0, 3):
+                    for b in range(0, 3):
+                        for u in range(1, 4):
+                            for v in range(1, 3):
+                                for w in range(v+1, 4):
                                     val1 = (3*a) + u
                                     val2 = (3*b) + v
                                     val3 = (3*b) + w
                                     lit1 = self.get_base_nine_num(val1,
                                                                   val2, d)
                                     lit2 = self.get_base_nine_num(val1,
-                                                                  val2, d)
-                                    literals = "-{0} -{1}".format(lit1, lit2)
+                                                                  val3, d)
+                                    literals = "-{0} -{1} 0\n".format(lit1,
+                                                                      lit2)
                                     line += literals
+        # print("first group: ", line) # for testing
+
+        line = ''
+        for d in range(1, 10):
+                for a in range(0, 3):
+                    for b in range(0, 3):
+                        for u in range(1, 3):
+                            for v in range(1, 4):
+                                for w in range(u+1, 4):
+                                    for t in range(1, 4):
+                                        val1 = (3*a) + u
+                                        val2 = (3*b) + v
+                                        val3 = (3*a) + w
+                                        val4 = (3*b) + t
+                                        lit1 = self.get_base_nine_num(val1,
+                                                                      val2, d)
+                                        lit2 = self.get_base_nine_num(val3,
+                                                                      val4, d)
+                                        literals = "-{0} -{1} 0\n".format(
+                                            lit1, lit2)
+                                        line += literals
+        # print("Second group: ", line) # for testing
 
     def format_output(self, inputFile):
         result = []
@@ -162,9 +171,11 @@ sudoku = SudokuParser()
 
 puzzle = sudoku.get_sudoku_puzzle()
 
-table = sudoku.create_variable_table(puzzle)
+#table = sudoku.create_variable_table(puzzle)
+sudoku.element_clauses()
 sudoku.row_clause()
-base_nine = sudoku.element_clauses()
+sudoku.sub_grid_clause()
+#base_nine = sudoku.element_clauses()
 encoded = sudoku.encode(puzzle)
 
 
